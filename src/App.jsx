6b5902e7c60ce489c2a7da730c6c30b453592aa7
printw <mcshishka200@gmail.com>
2022-12-31 14:52:35 +0300

    add localization and redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useEffect, useRef, useState } from 'react';
+import { Suspense, useEffect, useRef, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import AddIcon from '@mui/icons-material/Add';
+import i18n from 'i18next'
+import { initReactI18next, useTranslation } from 'react-i18next';
 
-function App() {
+const translationRu = { Backlog: 'Бэклог', 'To Do': 'Сделать', 'Ongoing': 'В работе', 'Done': 'Завершено', 'Add new task': 'Добавить новую задачу', 'Add': 'Добавить', 'Describe task...': 'Опишите задачу...', 'Delete?': "Удалить?", 'Yes': 'Да', 'No': 'Нет'}
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      ru: {translation: translationRu},
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: { escapeValue : false},
+  })
+
+const App = () => {
+  const { t } = useTranslation()
+  
   const [boards, setBoards] = useState([
-    {id: 1, title: 'Сделать', items:[]},
-    {id: 2, title: 'В работе', items:[]},
-    {id: 3, title: 'Завершено', items:[]},
-    {id: 4, title: 'На доработку', items:[]}
+    {id: 1, title: 'Backlog', items:[]},
+    {id: 2, title: 'To Do', items:[]},
+    {id: 3, title: 'Ongoing', items:[]},
+    {id: 4, title: 'Done', items:[]}
   ])
 
   useEffect(() => {
@@ -108,69 +125,72 @@ function App() {
   }
 
   return (
-    <div className="App overflow-y-hidden h-[100vh] select-none">
-      <Navbar/>
-        <div 
-          className='kanban_board gap-1 flex p-2 min-w-[800px] h-[93%]'
-        >
-          {boards.map(board => 
-            <div 
-              key={board.id} 
-              className={'board border-[5px] border-solid border-[#23272A] w-[25%] text-sm overflow-auto'}
-              onDragOver={(e) => dragOverHandler(e)}
-              onDrop={(e) => dropCardHandler(e, board)}
-              >
-
+    <Suspense fallback='Loading...'>
+      <div className="App overflow-y-hidden h-[100vh] select-none ">
+        <Navbar/>
+          <div 
+            className='gap-1 flex p-2 min-w-[800px] h-[93%]'
+          >
+            {boards.map(board => 
               <div 
-                className='text-xl text-center p-2 flex items-center justify-between select-none '
+                key={board.id} 
+                className={'board border-[5px] border-solid border-[#23272A] w-[25%] text-sm overflow-auto rounded-lg'}
+                onDragOver={(e) => dragOverHandler(e)}
+                onDrop={(e) => dropCardHandler(e, board)}
                 >
-                <p className='inline-block text-title'>{board.title}</p>
-                <label htmlFor={board.id} className="btn btn-accent modal-button w-[15px] h-[20px]"><AddIcon fontSize='small' style={{color: 'white'}}/></label>
-              </div>
-              <input type="checkbox" id={board.id} onChange={e => inputRefs.current[board.id - 1].focus()} className="modal-toggle" />
-              <div className="modal">
-                <div className="modal-box relative">
-                  <label htmlFor={board.id} className="btn btn-sm btn-circle absolute right-2 top-2" onClick={(e) => setCard('')}>✕</label>
-                  <h3 className="text-lg font-bold text-center p-5 mb-7">Добавить новую задачу</h3>
-                  <input ref={addToRefs} autoFocus type="text" placeholder="Задача" className="input input-bordered input-primary w-full max-w-xs" value={card} onChange={(e) => setCard(e.target.value)}/>
-                  <button className="btn btn-active btn-primary ml-5" onClick={(e) => addHandler(e, board)}>Добавить</button>
+                <div 
+                  className='text-xl text-center p-2 flex items-center justify-between select-none '
+                  >
+                  <p className='inline-block'>{t(board.title)}</p>
+                  <label htmlFor={board.id} className="btn modal-button w-[15px] h-[20px]"><AddIcon fontSize='small' style={{color: 'white'}}/></label>
                 </div>
-              </div>
-              
-              {board.items.map(item => 
-                  <div key={item.id} className="flex break-all ">
-                    <label htmlFor={item.id} className="modal-button item text-sm text-text mx-2 my-1 border-border border-[2px] p-2 w-[100%]">
-                      <div key={item.id}
-                      className="width-[100%] height-[100%]"
-                      onDragOver={(e) => dragOverHandler(e)}
-                      onDragStart={(e) => dragStartHandler(e, board, item)}
-                      onDrop={(e) => dropHandler(e, board, item)}
-                      draggable={true}
-                      >
-                        {item.title}
-                      </div>
-                    </label>
-                  
-                  <input type="checkbox" id={item.id} className="modal-toggle" />
-                  <div 
-                    className="modal"
-                    >
-                    <div 
-                      className="modal-box">
-                      <h3 className="font-bold text-lg">Удалить?</h3>
-                      <div className="modal-action">
-                        <label htmlFor={item.id} className="btn">Нет</label>
-                        <label htmlFor={item.id} className="btn" onClick={(e) => deleteHandler(e, board, item)}>Да</label>
-                      </div>
+                <input type="checkbox" id={board.id} onChange={e => inputRefs.current[board.id - 1].focus()} className="modal-toggle" />
+                <div className="modal">
+                  <div className="modal-box relative">
+                    <label htmlFor={board.id} className="btn btn-sm btn-circle absolute right-2 top-2" onClick={(e) => setCard('')}>✕</label>
+                    <h3 className="text-lg font-bold text-center p-5 mb-7">{t('Add new task')}</h3>
+                    <div className='flex flex-row'>
+                      <input ref={addToRefs} autoFocus type="text" placeholder={t('Describe task...')} className="input input-bordered input-primary w-full" value={card} onChange={(e) => setCard(e.target.value)}/>
+                      <button className="btn btn-active btn-primary ml-5" onClick={(e) => addHandler(e, board)}>{t('Add')}</button>
                     </div>
                   </div>
-
                 </div>
-                )}
-            </div>
-        )}
-        </div>
-    </div>
+                
+                {board.items.map(item => 
+                    <div key={item.id} className="flex break-all">
+                      <label htmlFor={item.id} className="modal-button item text-sm mx-2 my-1 border-[2px] p-2 w-[100%] cursor-pointer">
+                        <div key={item.id}
+                        className="width-[100%] height-[100%]"
+                        onDragOver={(e) => dragOverHandler(e)}
+                        onDragStart={(e) => dragStartHandler(e, board, item)}
+                        onDrop={(e) => dropHandler(e, board, item)}
+                        draggable={true}
+                        >
+                          {item.title}
+                        </div>
+                      </label>
+                    
+                    <input type="checkbox" id={item.id} className="modal-toggle"/>
+                    <div 
+                      className="modal"
+                      >
+                      <div 
+                        className="modal-box">
+                        <h3 className="font-bold text-lg">{t('Delete?')}</h3>
+                        <div className="modal-action">
+                          <label htmlFor={item.id} className="btn">{t('No')}</label>
+                          <label htmlFor={item.id} className="btn" onClick={(e) => deleteHandler(e, board, item)}>{t('Yes')}</label>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                  )}
+              </div>
+          )}
+          </div>
+      </div>
+    </Suspense>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,44 +3,8 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {
-      colors: 
-      {
-        "navbarTitle": 'hsl(var(--navbarTitle))',
-        "title": "hsl(var(--title))",
-        'border': 'hsl(var(--border))',
-        'text': 'hsl(var(--text))',
-        'addBtn': 'hsl(var(--addBtn))'
-      }
-    },
-  },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [
-        {
-        light: {
-          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
-          "base-100": "#DBE59E",
-          "primary": "#6aa84f",
-          'accent': '#6E8B58',
-          '--navbarTitle': '50, 53%, 34%',
-          '--title': '40, 100%, 26%',
-          '--border': '90, 21%, 26%',
-          '--text': '0, 0%, 0%',
-          '--addBtn': '94, 22%, 45%',
-        },
-        dark: {
-          ...require("daisyui/src/colors/themes")["[data-theme=dark]"],
-          "primary": "#5663F7",
-          'accent': '#1e2124',
-          '--navbarTitle': '0, 0%, 100%',
-          '--title': '0, 0%, 96%',
-          '--border': '221, 100%, 50%',
-          '--text': '0, 0%, 100%',
-          '--addBtn': '195, 7%, 11%'
-        }
-      }
-    ]
+    themes: ['light', 'dark'],
   },
 }
